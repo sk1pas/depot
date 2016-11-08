b19d096a624b6346b174830e1f825168c234bd40
rewert60 <rewert60@gmail.com>
2016-11-09 00:27:36 +0200

    A Smarter Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,23 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
-  validates_length_of :title, minimum: 10, message: "At least 10 characters!" 
+  validates_length_of :title, minimum: 10, message: "At least 10 characters!"
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for gif, jpg or png image'
   }
+
+  private
+    #ensure that there are no line items referencing this Product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
