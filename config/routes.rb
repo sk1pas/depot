441cbc9730a8cb68458e2007f4500a84e6e4ca87
rewert60 <rewert60@gmail.com>
2016-11-17 23:11:50 +0200

    Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
 
-
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
   resources :products do
     get :who_bought, on: :member
   end
-  #get 'store/index'
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+
 end
